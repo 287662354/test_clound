7c874f9abc3ffd1aad8d62b5aa31aab8e375ddf0
aiyun12 <1113713269@qq.com>
2026-06-28 14:10:02 +0800

    第二次修改

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
+/*
+ * @Author: aiyun12 1113713269@qq.com
+ * @Date: 2026-06-25 21:33:46
+ * @LastEditors: aiyun12 1113713269@qq.com
+ * @LastEditTime: 2026-06-28 14:09:45
+ * @FilePath: \test_clound\main.c
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "stdio.h"
 
 int main(void)
 {
 
-    printf("hello");
+    printf("hello++++草泥马  =================================================\n");
+      printf("hello++++草泥马  =================================================\n");
+
     return 0;
 
 }
